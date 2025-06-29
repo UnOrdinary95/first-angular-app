b0c0b6ec9efb62ad9fffe18f8d8dcf084e9d46c2
UnOrdinary95 <114882529+UnOrdinary95@users.noreply.github.com>
2025-06-29 23:18:11 +0200

    Setup Angular + HeaderComponent

---
+++ src/app/app.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  template: `
+    <h1>Welcome to {{title}}!</h1>
+    <p>Hello World</p>
+    <router-outlet />
+  `,
+  styles: [`
+    p{
+      background-color: red;
+    }
+    `],
+})
+export class App {
+  protected title = 'first-app';
+}
