88ad0e3efec037a5ded06199e7d7161a2abb9ff9
UnOrdinary95 <114882529+UnOrdinary95@users.noreply.github.com>
2025-06-30 11:29:40 +0200

    Creating routes in Angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Home } from './home/home';
 import { Header } from './components/header/header';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, Home, Header],
-  template: `
+    selector: 'app-root',
+    imports: [RouterOutlet, Header],
+    template: `
     <app-header/>
     <main>
-        <app-home/>
+        <router-outlet/>
     </main>
   `,
-  styles: [`
+    styles: [`
     main{
         padding: 16px;
     }
     `],
 })
 export class App {
-  protected title = 'first-app';
+    protected title = 'first-app';
 }
